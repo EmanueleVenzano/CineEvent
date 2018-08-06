dc3c23721c94185d74ff1e319a77a48163dbc820
s4202431 <37587902+s4202431@users.noreply.github.com>
2018-08-06 18:57:25 +0200

    Upload [2018-08-06_18-57-25]

--- Tesi1/app/src/main/java/nf/application/emanuele/tesi1/Locandine.java
+++ Tesi1/app/src/main/java/nf/application/emanuele/tesi1/Locandine.java
@@ -1,23 +1,29 @@
 package nf.application.emanuele.tesi1;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
+import com.google.ads.mediation.customevent.CustomEventAdapter;
+
 import java.lang.reflect.Array;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
-public class Locandine extends Activity implements View.OnClickListener {
+public class Locandine extends Activity implements AdapterView.OnItemClickListener {
     private ListView itemsListView;
-    private  TextView titleTextView;
-    class annunci {
-        String name;
-        String img;
-    }
+    private TextView titleTextView;
+    private List<Copertina> films = new LinkedList();
 
     @Override
     public void onCreate (Bundle savedInstanceState){
@@ -25,43 +31,40 @@ public class Locandine extends Activity implements View.OnClickListener {
         setContentView(R.layout.activity_locandine);
         itemsListView = (ListView) findViewById(R.id.itemsListView);
         titleTextView = (TextView) findViewById(R.id.titleTextView);
+        itemsListView.setOnItemClickListener(this);
         titleTextView.setText("Che magnifici Film!");
         Cinemas c = new Cinemas();
-        ArrayList<annunci> films = new ArrayList<>();
         for (int i=0; i< c.cinemas.size(); i++){
             for (int j=0; j<c.cinemas.get(i).films.size(); j++){
+                int controller=0;
                 for (int k=0; k<films.size(); k++){
-                    int controller=0;
                     if (films.get(k).name==c.cinemas.get(i).films.get(j).Titolo){
                         controller=1; break;
                     }
-                    if (controller == 0){
-                        annunci temp = new annunci();
-                        temp.name = c.cinemas.get(i).films.get(j).Titolo;
-                        temp.img = c.cinemas.get(i).films.get(j).immagine;
-                        films.add(temp);
+                }
+                if (controller == 0){
+                    Copertina temp = new Copertina();
+                    temp.name = c.cinemas.get(i).films.get(j).Titolo;
+                    try {
+                        temp.img = new URL(c.cinemas.get(i).films.get(j).immagine);
+                    }catch (MalformedURLException e){
+                        e.printStackTrace();
                     }
+                    films.add(temp);
                 }
             }
         }
 
-        ArrayList<HashMap<String, String>> data = new ArrayList<>();
-//trova ttutti i fillm
-        for (annunci item: films){
-            HashMap<String, String> map = new HashMap<>();
-            map.put("img", item.img);
-            map.put("title", item.name);
-            data.add(map);
-        }
-        int resource = R.layout.items_listview;
-        String[] from = {"img", "title"};
-        int[] to = {R.id.imgLocandine, R.id.titleLocandine};
-        SimpleAdapter adapter = new SimpleAdapter(this, data, resource, from, to);
+        CustomAdapter adapter = new CustomAdapter(this, R.layout.items_listview, films);
         itemsListView.setAdapter(adapter);
-
     }
 
-    public void onClick(View  view){
+    @Override
+    public void onItemClick(AdapterView<?> parent, View v, int position, long id){
 
+        Copertina item = films.get(position);
+        Intent intent = new Intent (this, InfoFilm.class);
+        intent.putExtra("name", item.name);
+        this.startActivity(intent);
     }
 }
